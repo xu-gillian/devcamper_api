9e55ce14d11e861f053bb9181eee88c0ba048417
gillian <gillianxu09@gmail.com>
2022-10-24 03:34:54 -0400

    creating routes

--- server.js
+++ server.js
@@ -6,6 +6,40 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// creating routes!
+// express knows everything -> including content type :o
+
+// this route should show all bootcamps
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+// gets specific bootcamp
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+// creates new boot camps
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+});
+
+// updates specific bootcamps
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+// updates specific bootcamps
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(
